f896f0d7bf1d9e935493b0fe49c3c239a75bae2e
Jason O'Neil <jason.oneil@gmail.com>
2017-07-19 09:11:30 +1000

    Add AsyncConnectionWrapper
    ...so we can use a sync Connection where an AsyncConnection is expected

--- src/sys/db/AsyncConnection.hx
+++ src/sys/db/AsyncConnection.hx
@@ -1,10 +1,11 @@
 package sys.db;
 
-import js.Error;
 import sys.db.ResultSet;
 
 /**
-Almost exactly the same as `sys.db.Manager` except that it uses callbacks to return asynchronous results.
+A database connection that expects operations to complete asynchcronously.
+
+Almost exactly the same as `sys.db.Connection` except that it uses callbacks to return asynchronous results.
 **/
 interface AsyncConnection {
 	function request( s : String, cb : ResultSetCallback ) : Void;
@@ -12,12 +13,87 @@ interface AsyncConnection {
 	function escape( s : String ) : String;
 	function quote( s : String ) : String;
 	function addValue( s : StringBuf, v : Dynamic ) : Void;
-	function lastInsertId( cb : Null<Error> -> Null<Int> -> Void ) : Void;
-	function dbName(  ) : String;
+	function lastInsertId( cb : Null<AsyncError> -> Null<Int> -> Void ) : Void;
+	function dbName() : String;
 	function startTransaction( cb : CompletionCallback ) : Void;
 	function commit( cb : CompletionCallback ) : Void;
 	function rollback( cb : CompletionCallback ) : Void;
 }
 
-typedef ResultSetCallback = Null<Error> -> ResultSet -> Void;
-typedef CompletionCallback = Null<Error> -> Void;
+/**
+A simple way to wrap any `sys.db.Connection` so that it can be used where a `sys.db.AsyncConnection` is expected.
+**/
+class AsyncConnectionWrapper implements AsyncConnection {
+	var syncCnx : Connection;
+	public function new( syncCnx : Connection ) {
+		this.syncCnx = syncCnx;
+	}
+
+	public function request( s : String, cb : ResultSetCallback ) : Void {
+		try {
+			cb(null, syncCnx.request(s));
+		} catch (e:Dynamic) {
+			cb(e, null);
+		}
+	}
+
+	public function close( cb : CompletionCallback ) : Void {
+		try {
+			syncCnx.close();
+			cb(null);
+		} catch (e:Dynamic) {
+			cb(e);
+		}
+	}
+
+	public function escape( s : String ) : String {
+		return syncCnx.escape(s);
+	}
+
+	public function quote( s : String ) : String {
+		return syncCnx.quote(s);
+	}
+
+	public function addValue( s : StringBuf, v : Dynamic ) : Void {
+		return syncCnx.addValue(s, v);
+	}
+
+	public function lastInsertId( cb : Null<AsyncError> -> Null<Int> -> Void ) : Void {
+
+	}
+
+	public function dbName() : String {
+		return syncCnx.dbName();
+	}
+
+	public function startTransaction( cb : CompletionCallback ) : Void {
+		try {
+			syncCnx.startTransaction();
+			cb(null);
+		} catch (e:Dynamic) {
+			cb(e);
+		}
+	}
+
+	public function commit( cb : CompletionCallback ) : Void {
+		try {
+			syncCnx.commit();
+			cb(null);
+		} catch (e:Dynamic) {
+			cb(e);
+		}
+	}
+
+	public function rollback( cb : CompletionCallback ) : Void {
+		try {
+			syncCnx.rollback();
+			cb(null);
+		} catch (e:Dynamic) {
+			cb(e);
+		}
+	}
+}
+
+typedef AsyncError = Dynamic;
+typedef ResultSetCallback = Null<AsyncError> -> ResultSet -> Void;
+typedef CompletionCallback = Null<AsyncError> -> Void;
