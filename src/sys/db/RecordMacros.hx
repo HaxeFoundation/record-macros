10d7767df74a1e9f318970662a82a0fff1e3a3e1
Jonas Malaco <jonas@protocubo.io>
2017-08-22 23:31:06 -0300

    Handle both typedef and abstract Null<T>
    Haxe 4 has switched to an abstract Null<T> [see: HaxeFoundation/haxe#6380].

--- src/sys/db/RecordMacros.hx
+++ src/sys/db/RecordMacros.hx
@@ -163,7 +163,7 @@ class RecordMacros {
 					return c;
 				csup = csup.t.get().superClass;
 			}
-		case TType(t, p):
+		case TType(t, p) | TAbstract(t, p):
 			var name = t.toString();
 			if( p.length == 1 && (name == "Null" || name == "sys.db.SNull") ) {
 				isNull = true;
@@ -211,6 +211,7 @@ class RecordMacros {
 			case "Int": DInt;
 			case "Float": DFloat;
 			case "Bool": DBool;
+			case "Null": isNull = true; return makeType(p[0]);
 			case _ if (!a.get().meta.has(':coreType')):
 				var a = a.get();
 #if macro
