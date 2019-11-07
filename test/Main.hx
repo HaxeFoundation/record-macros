a04089e1c7ed3c53900c7f00aad21bbd8d6fb084
Jonas Malaco <jonas@protocubo.io>
2019-11-07 19:27:24 -0300

    Run the SQLite tests on in-memory database unless --on-disk-sqlite
    This change makes half of all the tests nearly instantaneous, benefiting
    slower systems where each of the two test suites would take more than a
    few seconds.
    
    To restore the old behavior and use a persistent database – which can be
    useful when debugging certain types of issues – a new `--on-disk-sqlite`
    option is available.
    
    Note that while the path to the persistent SQLite database remains
    hardcoded – keep it simple, Steven – the filename extension has been
    changed to match common usage : `test.db`.

--- test/Main.hx
+++ test/Main.hx
@@ -1,27 +1,37 @@
 class Main {
 	static function main() {
-		var dbstr = Sys.args()[0];
 
 		var runner = new utest.Runner();
 		utest.ui.Report.create(runner);
 
-		runner.addCase(new SqliteTest("test.sqlite"));
+		var sqlitePath = ":memory:";
+		var mysqlParams = null;
 
-		if(dbstr != null) {
-			var dbreg = ~/([^:]+):\/\/([^:]+):([^@]*?)@([^:]+)(:[0-9]+)?\/(.*?)$/;
-			if (!dbreg.match(dbstr))
-				throw "Configuration requires a valid database attribute, format is : mysql://user:password@host:port/dbname";
-			var port = dbreg.matched(5);
-			var dbparams = {
-				user:dbreg.matched(2),
-				pass:dbreg.matched(3),
-				host:dbreg.matched(4),
-				port:port == null ? 3306 : Std.parseInt(port.substr(1)),
-				database:dbreg.matched(6),
-				socket:null
-			};
-			runner.addCase(new MysqlTest(dbparams));
+		for (arg in Sys.args()) {
+			switch arg {
+			case dbstr if (mysqlParams == null && dbstr.indexOf("mysql://") == 0):
+				var dbreg = ~/([^:]+):\/\/([^:]+):([^@]*?)@([^:]+)(:[0-9]+)?\/(.*?)$/;
+				if (!dbreg.match(dbstr))
+					throw "Configuration requires a valid database attribute, format is : mysql://user:password@host:port/dbname";
+				var port = dbreg.matched(5);
+				mysqlParams = {
+					user:dbreg.matched(2),
+					pass:dbreg.matched(3),
+					host:dbreg.matched(4),
+					port:port == null ? 3306 : Std.parseInt(port.substr(1)),
+					database:dbreg.matched(6),
+					socket:null
+				};
+			case "--on-disk-sqlite":
+				sqlitePath = "test.db";
+			case other:
+				throw 'Unsupported command line option or parameter: $other';
+			}
 		}
+
+		runner.addCase(new SqliteTest(sqlitePath));
+		if (mysqlParams != null)
+			runner.addCase(new MysqlTest(mysqlParams));
 
 		runner.run();
 	}
